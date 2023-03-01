49f4d085a55c44f870c5799afebcabf30f2ce1b0
Tatev Baghdasaryan <tatev.baghdasaryan.ml@gmail.com>
2023-03-01 16:57:49 +0400

    Added the experimental dataset, the main file for the project and the requirements file

---
+++ main.py
@@ -0,0 +1,47 @@
+import os
+import cv2
+import numpy as np
+from modules.stitch import match_pairs
+from modules.change_detection import Change
+from modules.DeepLabV3_segmentation.DeepLabV3 import deeplabv3_segment
+
+
+def main():
+    img_dir = './images/'
+    names = [file for file in sorted(os.listdir(os.path.join(img_dir + 'A/')))]
+    
+    for name in names:
+        img1 = cv2.imread(os.path.join(img_dir + 'A/' + name))
+        img2 = cv2.imread(os.path.join(img_dir + 'B/' + name))
+
+        mask_of_first_img = deeplabv3_segment(img1)
+        mask_of_second_img = deeplabv3_segment(img2) 
+
+        layers_of_first_img = []   #call a function from DeepLab in the future!!
+        layers_of_second_img = []  #call a function from DeepLab in the future!!
+
+        keypoints = match_pairs([img1, img2]) 
+        keypoints1, keypoints2, matches = keypoints[0], keypoints[1], keypoints[2]
+
+        object_keypoints_A = []
+        object_keypoints_B = []
+
+        for a_layer in layers_of_first_img:
+            keys = []
+            for el in keypoints1:
+                check = a_layer[el[1], el[0]]
+                if not (check[0] < 50 and check[1] < 50 and check[2] < 50):
+                    keys.append(keypoints1.index(el)) 
+                object_keypoints_A.append(keys)
+
+        for a_layer in layers_of_second_img:
+            keys = []
+            for el in keypoints2:
+                check = a_layer[el[1], el[0]]
+                if not (check[0] < 50 and check[1] < 50 and check[2] < 50):
+                    keys.append(keypoints1.index(el)) 
+                object_keypoints_B.append(keys)
+
+
+if __name__ == "__main__":
+    main() 
