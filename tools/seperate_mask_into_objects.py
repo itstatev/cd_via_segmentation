ef224b40c92a20a3e8942538220cf3383f2776a8
ArmanFahradyan <armanfahradyan02@gmail.com>
2023-03-03 10:27:48 +0400

    changed tool/seperate_mask_into_objects file from recursive to iterative

--- tools/seperate_mask_into_objects.py
+++ tools/seperate_mask_into_objects.py
@@ -1,14 +1,6 @@
 import os 
 import cv2
 import argparse
-import numpy as np
-
-
-parser = argparse.ArgumentParser()
-parser.add_argument('-s', "--source", type=str, required=True, help="path of the image or directory containing images")
-parser.add_argument('-d', "--destination", type=str, default='', help="path of the destination directory. \
-                                                                       Not storing results if destination is not provided")
-args = parser.parse_args()
 
 
 def get_name_and_extension(filename):
@@ -18,36 +10,35 @@ def get_name_and_extension(filename):
 
 
 def seperate_a_mask_into_objects(mask, mask_path, destination):
-    if not mask:
+    if  mask is None:
         mask = cv2.imread(mask_path)
+    if len(mask.shape) == 3:
         mask = cv2.cvtColor(mask, cv2.COLOR_BGR2GRAY)
     
-    # mask[mask > 123] = 255
-    # mask[mask <= 123] = 0
 
     def detect_islands(mask):
-        # deep_copy = lambda arg: [deep_copy(el) if isinstance(el, list) else el for el in arg]
-        # mat = deep_copy(mat)
-        # m = len(mat)
-        # n = len(mat[0])
         m, n = mask.shape
         valid_row = lambda x: 0 <= x < m
         valid_col = lambda x: 0 <= x < n
+
         def bfs(mask, i, j, color):
-            if mask[i, j] == color:
+            assert color != 254
+            queue = []
+            queue.append((i, j))
+            while queue:
+                i, j = queue[-1]
+                queue.pop()
                 mask[i, j] = 254
-                if valid_row(i-1):
-                    bfs(mask, i-1, j, color)
-                if valid_row(i+1):
-                    bfs(mask, i+1, j, color)
-                if valid_col(j-1):
-                    bfs(mask, i, j-1, color)
-                if valid_col(j+1):
-                    bfs(mask, i, j+1, color)
-        # color = 0
+                if valid_row(i-1) and mask[i-1, j] == color:
+                    queue.append((i-1, j))
+                if valid_row(i+1) and mask[i+1, j] == color:
+                    queue.append((i+1, j))
+                if valid_col(j-1) and mask[i, j-1] == color:
+                    queue.append((i, j-1))
+                if valid_col(j+1) and mask[i, j+1] == color:
+                    queue.append((i, j+1))
 
         masks = []
-
         for i in range(m):
             for j in range(n):
                 if mask[i, j] != 0:
@@ -89,4 +80,9 @@ def seperate_masks_into_objects(source, destination):
 
 
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-s', "--source", type=str, required=True, help="path of the image or directory containing images")
+    parser.add_argument('-d', "--destination", type=str, default='', help="path of the destination directory. \
+                                                                        Not storing results if destination is not provided")
+    args = parser.parse_args()
     seperate_masks_into_objects(args.source, args.destination)
